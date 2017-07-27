b9bb8db165cb7d91b4973b989362f9136592ec45
Natalia Garrido <natgramirez@outlook.com>
2017-07-27 19:35:49 -0400

    agregando archivos y dependencias

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var minifyCSS = require('gulp-minify-css');
+var webserver = require('gulp-webserver');
